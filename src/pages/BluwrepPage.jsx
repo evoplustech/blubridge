8394ee602745249d5bef61bd64c3956c85bafb9d
vijayan <hadwickcrispin@gmail.com>
2025-11-27 02:09:48 +0530

    updated the REsearch Page

--- src/pages/BluwrepPage.jsx
+++ src/pages/BluwrepPage.jsx
@@ -40,8 +40,16 @@ const BluwrepPage = () => {
             </span>
         </h1>
 <div class="blb-btn-group">
-    <Link to="/Research"  class="blb-btn">Read the Paper</Link>
-    <Link to="/Careers" class="blb-btn"> Work on this at BluBridge</Link>
+   <Link to="/Research"  class="paper-btn">
+    <svg class="paper-icon" xmlns="http://www.w3.org/2000/svg" 
+         width="20" height="20" fill="white" viewBox="0 0 24 24">
+        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 
+        2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/>
+        <path d="M14 2v6h6"/>
+    </svg>
+    Read the Paper
+    </Link>
+  
 </div>
         <div class="blb-content">
 
